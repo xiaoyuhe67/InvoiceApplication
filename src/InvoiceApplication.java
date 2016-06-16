ffee0d8114e792c68f274127879d83c86be9992f
xiaoyuhe67 <xiaoyuhe67@github.com>
2016-06-16 15:33:19 -0400

    Invoice with exception

--- src/InvoiceApplication.java
+++ src/InvoiceApplication.java
@@ -1,48 +1,61 @@
 import java.util.Scanner;
 
 public class InvoiceApplication {
-	
+
 	static Scanner keyboard=new Scanner(System.in);
-	
+
 	public static void main(String[] args)
 	{
-		double taxrate=0;
-		double price=1;
-		int pricenumber=0;
-		System.out.print("Tax Rate ? : ");
-		taxrate=keyboard.nextDouble();
-		System.out.println("");
-		
-		double pricearr[]=new double[100];
-		
-		double subtotal=0;
-		
-		while(price>0)
-		{
-			pricenumber=pricenumber+1;
-			System.out.print("Price # "+pricenumber+ " : ");
-			price=keyboard.nextDouble();
-			pricearr[pricenumber-1]=price;
-			System.out.println("");
-			
-			if (price==0.0)
-			{
-				System.out.println("-----------------------");
-				System.out.println("Receipt");
-				for(int i=0;i<=pricenumber-2;i++)
-				{
-					subtotal=pricearr[i]+subtotal;
-					System.out.println(String.format("%.02f", pricearr[i]));
-				}
+		try{
+			double taxrate=0;
+			double price=1;
+			int pricenumber=0;
+			System.out.print("Tax Rate ? : ");
 				
-				System.out.println(subtotal+" subtotal");
-				System.out.println((subtotal*taxrate)+" tax");
-				System.out.println(String.format("%.02f", (subtotal*(1+taxrate)))+" grand total");
-				break;
+			taxrate=keyboard.nextDouble();
+			System.out.println("");
+
+			double pricearr[]=new double[100];
+
+			double subtotal=0;
+
+			while(price>0)
+			{
+				pricenumber=pricenumber+1;
+				System.out.print("Price # "+pricenumber+ " : ");
+				price=keyboard.nextDouble();
+				pricearr[pricenumber-1]=price;
+				System.out.println("");
+
+				if (price==0.0)
+				{
+					System.out.println("-----------------------");
+					System.out.println("Receipt");
+					for(int i=0;i<=pricenumber-2;i++)
+					{
+						subtotal=pricearr[i]+subtotal;
+						System.out.println(String.format("%.02f", pricearr[i]));
+					}
+
+					System.out.println(subtotal+" subtotal");
+					System.out.println((subtotal*taxrate)+" tax");
+					System.out.println(String.format("%.02f", (subtotal*(1+taxrate)))+" grand total");
+					break;
+				}
 			}
+		}			
+		catch(NumberFormatException e)
+		{
+			System.out.println(e.getMessage());
+		}catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+
+		
+		finally{
+			System.out.println("Close the program! Bye");
 		}
 		
-		
 	}
-
 }
